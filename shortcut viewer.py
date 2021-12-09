a415e29a934943e1c00b731431b7ab06ce0709dd
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2021-12-09 09:02:08 +0200

    initial commit

--- shortcut viewer.py
+++ shortcut viewer.py
@@ -1,14 +1,26 @@
 from pynput import keyboard
 from plyer import notification
+from time import sleep
 
 
 current = []
 comb = []
 is_possible = False
 
+notification.notify(title="Shortcut viewer is on.",
+                    message="To stop it press shift + esc.",
+                    app_icon="shortcut-logo.ico")
+
 
 def execute():
     global comb
+    if "shift" in comb and "esc" in comb and len(comb) == 2:
+        notification.notify(title="Shortcut viewer is closing",
+                            message="Thank you for using the app.",
+                            app_icon="shortcut-logo.ico",
+                            timeout=1)
+        sleep(5)
+        raise SystemExit
     comb = list(set(comb))
     comb.sort(key=len, reverse=True)
     title = ' + '.join(comb)
@@ -23,7 +35,6 @@ def execute():
 def on_press(key):
     global is_possible
     new_key = str(key)
-    print(new_key)
     new_key = new_key.replace("Key.", "")
     new_key = new_key.replace("_l", "")
     new_key = new_key.replace("_r", "")
@@ -33,21 +44,25 @@ def on_press(key):
         is_possible = True
     elif is_possible:
         comb.append(new_key)
-        if not (comb[-1] == "ctrl" or comb[-1] == "shift" or comb[-1] == "alt" or comb[-1] == "cmd"):
-            comb[-1] = str(comb[-1]).replace("\\", "0", 1)
-            comb[-1] = str(comb[-1]).replace("'", "")
-            if comb[-1] == "<191>":
-                comb[-1] = "/"
-            elif len(comb[-1]) > 1 and not str(comb[-1]).isalpha() and "f" not in str(comb[-1]):
-                if comb[-1] == "0t":
-                    comb[-1] = "0x09"
-                comb[-1] = chr(int(comb[-1], 16) + 64)
-            is_possible = False
-            execute()
+        comb[-1] = str(comb[-1]).replace("\\", "0", 1)
+        comb[-1] = str(comb[-1]).replace("'", "")
+        if comb[-1] == "<191>":
+            comb[-1] = "/"
+        elif len(comb[-1]) > 1 and not str(comb[-1]).isalpha() and "f" not in str(comb[-1]):
+            if comb[-1] == "0t":
+                comb[-1] = "0x09"
+            comb[-1] = chr(int(comb[-1], 16) + 64)
+        is_possible = False
+        execute()
 
 
 def on_release(key):
-    current.pop(0)
+    global is_possible
+    if not current:
+        execute()
+        is_possible = False
+    else:
+        current.pop(0)
 
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
